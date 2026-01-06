3e41d0215d3a6e0e10befb23a2e63091e6bc2365
tcguoxing <guoxing_sun@asdc.com.cn>
2026-01-06 18:03:01 +0800

    MOD：创建一个基本项目；

---
+++ react_jike/src/router/index.js
@@ -0,0 +1,7 @@
+import {createBrowserRouter} from "react-router-dom";
+import App from "@/App";
+
+export default createBrowserRouter([
+    {path: '/', element: <App/>},
+    {path: '/about', element:<div>这是About页</div>}
+])
